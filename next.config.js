279af3ab3287f31f6c20d4f9eae893f57bc2d25f
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-03-25 20:43:49 +0000

    fix next

--- next.config.js
+++ next.config.js
@@ -36,23 +36,40 @@ const nextConfig = {
   },
 };
 
+// module.exports = nextConfig;
+
+// Injected content via Sentry wizard below
+
 const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(module.exports, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
   org: "school-of-code-73",
   project: "blackjack",
-  sentryBuildOptions: {
-    sourcemaps: {
-      deleteSourcemapsAfterUpload: true,
-      disable: false,
-    },
-    widenClientFileUpload: true,
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-    tunnelRoute: "/monitoring",
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
   },
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  automaticVercelMonitors: true,
 });
